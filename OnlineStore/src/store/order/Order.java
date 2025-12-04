72540146c52f4eb25d045203e997d1cccd8579b6
shiraasaraf <shiraasaraf120@icloud.com>
2025-12-04 19:58:15 +0200

    Add .gitignore and updating the constructor and adding equal and toString methods in the Order class

--- OnlineStore/src/store/order/Order.java
+++ OnlineStore/src/store/order/Order.java
@@ -5,38 +5,120 @@
  */
 
 package store.order;
+
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import store.cart.CartItem;
 import store.core.Persistable;
 
-
-public class Order implements Persistable{
+/**
+ * Class representing an order made by a customer.
+ * Contains order details such as ID, items, total amount and status.
+ */
+public class Order implements Persistable {
     private int orderID;
     private List<CartItem> items;
     private double totalAmount;
     private OrderStatus status;
 
-
-    public Order(List<CartItem> items, double totalAmount) {
-        OrderStatus status = OrderStatus.NEW;
-        //TODO
+    /**
+     * Constructs a new Order instance with given ID, items and total amount.
+     * Status is initialized to NEW.
+     *
+     * @param orderID unique identifier of the order
+     * @param items list of CartItem objects in the order (copied internally)
+     * @param totalAmount total cost of the order
+     */
+    public Order(int orderID, List<CartItem> items, double totalAmount) {
+        this.orderID = orderID;
+        this.items = new ArrayList<>(items);
+        this.totalAmount = totalAmount;
+        this.status = OrderStatus.NEW;
     }
+
+    /**
+     * Saves the order to a file at the specified path.
+     * Implementation is pending.
+     *
+     * @param path file path to save the order data
+     */
     public void saveToFile(String path){
         //TODO
     }
 
+    /**
+     * Marks the order as paid.
+     *
+     * @return true always, indicating the status was set to PAID
+     */
     public boolean pay() {
-        //TODO
-        return false;
+        this.status = OrderStatus.PAID;
+        return true;
     }
+
+    /**
+     * Marks the order as shipped if it has been paid.
+     *
+     * @return true if status changed to SHIPPED, false otherwise
+     */
     public boolean ship(){
+        if(this.status == OrderStatus.PAID){
+            this.status = OrderStatus.SHIPPED;
+            return true;
+        }
         return false;
     }
 
+    /**
+     * Marks the order as delivered if it has been shipped.
+     *
+     * @return true if status changed to DELIVERED, false otherwise
+     */
     public boolean deliver(){
+        if(this.status == OrderStatus.SHIPPED){
+            this.status = OrderStatus.DELIVERED;
+            return true;
+        }
         return false;
     }
 
-    //TODO equals toString
+    /**
+     * Returns a string representation of the order,
+     * including order ID, status, total amount and item details.
+     *
+     * @return formatted string describing the order
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Order ID: ").append(orderID).append("\n");
+        sb.append("Status: ").append(status).append("\n");
+        sb.append("Total Amount: ").append(String.format("%.2f", totalAmount)).append("\n");
+        sb.append("Items:\n");
+
+        for (CartItem item : items) {
+            sb.append(" - ").append(item.toString()).append("\n");
+        }
+
+        return sb.toString();
+    }
+
+    /**
+     * Compares this order to another object for equality.
+     * Two orders are equal if they have the same orderID.
+     *
+     * @param obj the object to compare to
+     * @return true if obj is an Order with the same orderID, false otherwise
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj){
+            return true;
+        }
+        if (obj instanceof Order) {
+            return orderID == ((Order) obj).orderID;
+        }
+        return false;
+    }
 }
